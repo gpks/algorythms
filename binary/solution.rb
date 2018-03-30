4a4872f626852c53d8fa6c0d38f461be1a913419
Małgorzata Ksionek <meksionek@gmail.com>
2018-03-30 22:46:18 +0200

    Make it simplet

--- binary/solution.rb
+++ binary/solution.rb
@@ -1,17 +1,17 @@
 class Algo
   def solution(n)
     binary = "%b" % n
-    solution = []
+    solution = 0
     current = 0
     binary.each_char.with_index do |bin, i|
       if bin == '0'
         current += 1
       else
-        solution << current
+        solution = current if current > solution
         break if current > binary[i+1..-1].size
         current = 0
       end
     end
-    solution.max
+    solution
   end
 end
