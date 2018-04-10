daed393780588bdb692eba45b1919bd08c25a55b
Małgorzata Ksionek <meksionek@gmail.com>
2018-04-10 11:46:57 +0200

    Add xor solution

--- odd_occurences/odd_occurences.rb
+++ odd_occurences/odd_occurences.rb
@@ -1,12 +1,23 @@
 class Algo
   def solution(arr)
     array = arr.collect { |item| [item, 0] } # =>O(n)
-    help = Hash[array] # => O(n)
+    help = Hash[array] # => O(n)/// space complexity n
     result = nil
     arr.each do |key| # => O(n)
-      help[key] += 1
+      help[key] += 1 # => O(logn)
       result = key if help[key] == 1
     end
     result
   end
+
+  def solution_xor(arr)
+    res = 0
+    arr.each do |num|
+      res = num ^ res
+    end
+    res
+  end
 end
+
+#xor 2 zapisy binarne bit po bicie - rowne 0, inne 1
+
--- odd_occurences/odd_occurences_test.rb
+++ odd_occurences/odd_occurences_test.rb
@@ -11,4 +11,8 @@ class AlgoTest < Minitest::Test
   def test_long_array
     assert_equal 3, Algo.new.solution([5,1,5,1,6,2,6,2,10,11,12,13,14,15,16,17,18,19,20,21,22,10,11,12,13,14,15,16,17,18,19,20,21,22,3])
   end
+
+  def test_long_array_xor
+    assert_equal 3, Algo.new.solution_xor([5,1,5,1,6,2,6,2,10,11,12,13,14,15,16,17,18,19,20,21,22,10,11,12,13,14,15,16,17,18,19,20,21,22,3])
+  end
 end
